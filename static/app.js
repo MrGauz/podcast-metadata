01856dd8316ebd56ce842c62ef1debc20f5eb169
Kirill Gringauz <kirill.gringauz@kodex-ai.com>
2024-01-30 00:11:29 +0100

    Remove artwork button

--- static/app.js
+++ static/app.js
@@ -2,10 +2,10 @@ const alertPlaceholder = document.getElementById('alert-placeholder');
 const showAlert = (message, type) => {
     let wrapper = document.createElement('div');
     wrapper.innerHTML = [
-      `<div class="alert alert-${type} alert-dismissible" role="alert">`,
-      `   <div>${message}</div>`,
-      `   <button type="button" class="btn-close" data-bs-dismiss="alert"></button>`,
-      `</div>`
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        `   <button type="button" class="btn-close" data-bs-dismiss="alert"></button>`,
+        `</div>`
     ].join('');
     alertPlaceholder.append(wrapper);
 
@@ -21,7 +21,7 @@ const previewImage = () => {
     if (fileInput.files && fileInput.files[0]) {
         var reader = new FileReader();
 
-        reader.onload = function(e) {
+        reader.onload = function (e) {
             preview.src = e.target.result;
             preview.classList.remove('d-none');
         };
@@ -37,45 +37,45 @@ const loadPreset = (e, endpoint, presetId) => {
     e.preventDefault();
 
     fetch(endpoint + '?preset-id=' + presetId)
-    .then(response => {
-        if (response.status === 200) {
-            return response.json();
-        } else {
-            response.text().then(text => {
-                showAlert(text, 'danger');
-            });
-        }
-    })
-    .then(data => {
-        if (data) {
-            for (const [key, value] of Object.entries(data)) {
-                var input = document.getElementById(key.replace('_', '-'));
+        .then(response => {
+            if (response.status === 200) {
+                return response.json();
+            } else {
+                response.text().then(text => {
+                    showAlert(text, 'danger');
+                });
+            }
+        })
+        .then(data => {
+            if (data) {
+                for (const [key, value] of Object.entries(data)) {
+                    var input = document.getElementById(key.replace('_', '-'));
 
-                if (input && value) {
-                    if (key === 'artwork') {
-                        preview = document.getElementById('artwork-preview');
-                        preview.src = value;
-                        preview.style.display = 'block';
-                        document.getElementById('artwork-name').value = value;
-                    } else {
-                        input.value = value;
+                    if (input && value) {
+                        if (key === 'artwork') {
+                            preview = document.getElementById('artwork-preview');
+                            preview.src = value;
+                            preview.style.display = 'block';
+                            document.getElementById('artwork-name').value = value;
+                        } else {
+                            input.value = value;
+                        }
                     }
                 }
             }
-        }
-    })
-    .catch(error => {
-        console.error('Network error: ', error);
-        showAlert('Network error', 'warning');
-    });
+        })
+        .catch(error => {
+            console.error('Network error: ', error);
+            showAlert('Network error', 'warning');
+        });
 }
 
-document.getElementById('reset-button').addEventListener('click', function() {
+document.getElementById('reset-button').addEventListener('click', function () {
     document.getElementById('metadata-form').reset();
     document.getElementById('artwork-preview').style.display = 'none';
 });
 
-document.getElementById('submit-preset').addEventListener('click', function() {
+document.getElementById('submit-preset').addEventListener('click', function () {
     const originalText = this.innerHTML;
     this.setAttribute('disabled', 'disabled');
     this.innerHTML = '<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>Loading...';
@@ -92,21 +92,26 @@ document.getElementById('submit-preset').addEventListener('click', function() {
         method: 'POST',
         body: formData
     })
-    .then(response => {
-        if (response.status === 201) {
-            showAlert('Preset created successfully', 'success');
-        } else {
-            response.text().then(text => {
-                showAlert(text, 'danger');
-            });
-        }
-    })
-    .then(() => {
-        this.removeAttribute('disabled');
-        this.innerHTML = originalText;
-    })
-    .catch(error => {
-        console.error('Network error: ', error);
-        showAlert('Network error, try again later', 'warning');
-    });
+        .then(response => {
+            if (response.status === 201) {
+                showAlert('Preset created successfully', 'success');
+            } else {
+                response.text().then(text => {
+                    showAlert(text, 'danger');
+                });
+            }
+        })
+        .then(() => {
+            this.removeAttribute('disabled');
+            this.innerHTML = originalText;
+        })
+        .catch(error => {
+            console.error('Network error: ', error);
+            showAlert('Network error, try again later', 'warning');
+        });
+});
+
+document.getElementById('remove-artwork').addEventListener('click', function () {
+    document.getElementById('artwork-preview').classList.add('d-none');
+    document.getElementById('artwork').value = '';
 });
